353f10b0a6bdc0c432d67c52782bc05dfb716d16
User <majedhabli2020@gmail.com>
2023-08-21 21:18:04 +0300

    Server <> Grouped apis.

--- Server-YourDailyCookBook/routes/api.php
+++ Server-YourDailyCookBook/routes/api.php
@@ -4,10 +4,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-Route::controller(AuthController::class)->group(function () {
-    Route::post('login', 'login');
-    Route::post('register', 'register');
+
+Route::group(['prefix' => 'guest'], function(){
+    Route::post("login", [AuthController::class, "login"]);
+    Route::post("register", [AuthController::class, "register"]);
+    Route::get("unauthorized", [AuthController::class, "unauthorized"])->name("unauthorized");
+});
+
+Route::group(['prefix' => 'user', 'middleware' => 'auth:api'], function(){
     Route::post('logout', 'logout');
     Route::post('refresh', 'refresh');
-
 });

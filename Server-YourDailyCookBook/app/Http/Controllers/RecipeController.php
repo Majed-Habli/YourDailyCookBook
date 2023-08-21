ff5d89e641efc83efe089f67830472ca68067924
User <majedhabli2020@gmail.com>
2023-08-22 02:20:20 +0300

    Server <> Created a create a get recipe apis.

---
+++ Server-YourDailyCookBook/app/Http/Controllers/RecipeController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use App\Models\Recipe;
+use Auth;
+
+class RecipeController extends Controller
+{
+    function CreateRecipe(Request $request){
+
+        $fileNameExt = $request->file('image_url')->getClientOriginalName();
+        $fileName = pathinfo($fileNameExt, PATHINFO_FILENAME);
+        $fileExt = $request->file('image_url')->getClientOriginalExtension();
+        $fileNameToStore = $fileName.'_'.time().'.'.$fileExt;
+        $pathToStore = $request->file('image_url')->storeAs('public/recipe_images',$fileNameToStore);
+
+        $user = Auth::user();
+
+        $recipe = new Recipe;
+        $recipe->user_id = $user->id;
+        $recipe->name = $request->name;
+        $recipe->cousine = $request->cousine;
+        $recipe->ingredients = $request->ingredients;
+        $recipe->image_url = $fileNameToStore;
+        $recipe->save();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $recipe
+        ]);
+    }
+
+    function getRecipes(){
+        $user = Auth::user();
+        $user_id = $user->id;
+        $recipe = $user->Recipes()->get();
+
+        return response()->json([
+            'status' => 'success',
+            'user_id' => $user_id,
+            'data' => $recipe
+        ]);
+    }
+}
--- Server-YourDailyCookBook/routes/api.php
+++ Server-YourDailyCookBook/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RecipeController;
 
 
 Route::group(['prefix' => 'guest'], function(){
@@ -14,4 +15,6 @@ Route::group(['prefix' => 'guest'], function(){
 Route::group(['prefix' => 'user', 'middleware' => 'auth:api'], function(){
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh',[AuthController::class, 'refresh']);
+    Route::post('create_recipe', [RecipeController::class, 'CreateRecipe']);
+    Route::get('get_recipe', [RecipeController::class, 'getRecipes']);
 });

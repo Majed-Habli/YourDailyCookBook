79e7c918cf70524bbc4839b9e5196d295c8ca113
User <majedhabli2020@gmail.com>
2023-08-22 09:12:27 +0300

    Client <> Updated the signin page styling.

--- daily-cook-book/src/pages/signin/signin.jsx
+++ daily-cook-book/src/pages/signin/signin.jsx
@@ -8,6 +8,14 @@ const Signin = () => {
     return(
         <div className={styles.container}>
             <div className={styles.input_form}>
+                <div className={styles.title_container}>
+                    <div className={styles.title}>Daily Cook Book</div>
+                    <div>Welcome Back</div>
+                    <div className={styles.slogan}>
+                        sign in and get cooking.
+                    </div>
+                </div>
+
                 <div className={styles.row}>
                     <label>Email:</label>
                     <input type="text"  value={email} onChange={ e => setEmail(e.target.value)} placeholder="Email"/>
@@ -19,6 +27,7 @@ const Signin = () => {
                 <div className={styles.button_container}>
                     <button>Login</button>
                 </div>
+                <div className={styles.CTA}>Don't have an account? <span>Sign Up</span></div>
             </div>
         </div>
     )

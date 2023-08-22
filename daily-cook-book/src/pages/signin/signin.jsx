eb431552589ca1e687a4b1b16865d55750870778
User <majedhabli2020@gmail.com>
2023-08-22 08:54:57 +0300

    Client <> Updated the signin page styling.

--- daily-cook-book/src/pages/signin/signin.jsx
+++ daily-cook-book/src/pages/signin/signin.jsx
@@ -8,8 +8,14 @@ const Signin = () => {
     return(
         <div className={styles.container}>
             <div className={styles.input_form}>
-                <input type="text"  value={email} onChange={ e => setEmail(e.target.value)} placeholder="Email"/>
-                <input type="password"  value={password} onChange={ e => setPassword(e.target.value)} placeholder="Password"/>
+                <div className={styles.row}>
+                    <label>Email:</label>
+                    <input type="text"  value={email} onChange={ e => setEmail(e.target.value)} placeholder="Email"/>
+                </div>
+                <div className={styles.row}>
+                    <label>password:</label>
+                    <input type="password"  value={password} onChange={ e => setPassword(e.target.value)} placeholder="Password"/>
+                </div>
                 <div className={styles.button_container}>
                     <button>Login</button>
                 </div>

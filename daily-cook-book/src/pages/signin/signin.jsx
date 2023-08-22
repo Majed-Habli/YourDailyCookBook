7325953358b67896a15307c0a627d4d49d24b5a8
User <majedhabli2020@gmail.com>
2023-08-22 08:45:55 +0300

    Client <> Created initial routing and signin page with basic styling.

--- daily-cook-book/src/pages/signin/signin.jsx
+++ daily-cook-book/src/pages/signin/signin.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from './signin.module.css';
 
 const Signin = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return(
         <div className={styles.container}>
-            hello
+            <div className={styles.input_form}>
+                <input type="text"  value={email} onChange={ e => setEmail(e.target.value)} placeholder="Email"/>
+                <input type="password"  value={password} onChange={ e => setPassword(e.target.value)} placeholder="Password"/>
+                <div className={styles.button_container}>
+                    <button>Login</button>
+                </div>
+            </div>
         </div>
     )
 }
